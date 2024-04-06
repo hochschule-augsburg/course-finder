f3c79f6922cddb58fe521a2cf14949515b6e06fa
singhraj <Rajbir.Singh@hs-augsburg.de>
2024-04-06 18:05:59 +0200

    clean up

---
+++ packages/ui/src/pages/Test.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { trpc } from '@/api/trpc'
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+async function login() {
+  console.log(
+    await trpc.auth.login.query({
+      password: password.value,
+      username: username.value,
+    }),
+  )
+}
+</script>
+<template>
+  <h1>This page is only for testing purposes</h1>
+  <VTextField v-model:model-value="username" type="text" />
+  <VTextField v-model:model-value="password" type="password" />
+  <button @click="login">login</button>
+</template>
