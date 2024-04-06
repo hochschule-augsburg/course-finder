05fb72d717352f247a19c78b95565501b1c2ffe8
singhraj <Rajbir.Singh@hs-augsburg.de>
2024-04-06 18:35:59 +0200

    add pinia

--- packages/ui/src/plugins/index.ts
+++ packages/ui/src/plugins/index.ts
@@ -3,8 +3,9 @@ import type { App } from 'vue'
 import { VueQueryPlugin } from 'vue-query'
 
 import router from '../router'
+import pinia from './pinia'
 import vuetify from './vuetify'
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router).use(VueQueryPlugin)
+  app.use(vuetify).use(pinia).use(router).use(VueQueryPlugin)
 }
