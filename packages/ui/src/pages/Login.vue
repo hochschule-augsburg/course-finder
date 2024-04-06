667208ce74295000d1e9f287beb8e1dcfc3055f9
singhraj <Rajbir.Singh@hs-augsburg.de>
2024-04-06 18:06:17 +0200

    define pages

---
+++ packages/ui/src/pages/Login.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <h1>This is the Login View</h1>
+</template>
---
+++ packages/ui/src/pages/Main.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <h1>This is the Main View</h1>
+</template>
