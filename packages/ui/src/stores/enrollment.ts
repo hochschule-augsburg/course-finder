ad10f1ad02d52b4bec0c1b3c24b17948bc0e3f11
singhraj <Rajbir.Singh@hs-augsburg.de>
2024-04-07 14:01:18 +0200

    define subject types

--- packages/ui/src/stores/enrollment.ts
+++ packages/ui/src/stores/enrollment.ts
@@ -1,6 +1,28 @@
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
+export type Meeting = {
+  from: Date
+  to: Date
+}
+
+export type Subject = {
+  cp: number
+  description: string
+  id: string
+  info: string
+  maxTnm: number
+  meetings: Meeting[] | undefined
+  minTnm: number
+  moduleMan: string
+  name: string
+  prof: string
+  sws: number
+  weekly: Meeting | undefined
+}
+
+export type SubjectProp = keyof Subject
+
 export const useEnrollmentStore = defineStore('enrollment', () => {
   const count = ref(0)
   const name = ref('Eduardo')
