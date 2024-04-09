4d0e039dff5cebfd345902ee9c1b97de543bb213
Niklas Sirch <sirniklax@gmail.com>
2024-04-09 19:52:13 +0200

    remove vue-query

--- packages/ui/src/plugins/index.ts
+++ packages/ui/src/plugins/index.ts
@@ -1,12 +1,10 @@
 import type { App } from 'vue'
 
-import { VueQueryPlugin } from 'vue-query'
-
 import router from '../router'
 import { i18n } from './i18n'
 import pinia from './pinia'
 import vuetify from './vuetify'
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(pinia).use(router).use(VueQueryPlugin).use(i18n)
+  app.use(vuetify).use(pinia).use(router).use(i18n)
 }
