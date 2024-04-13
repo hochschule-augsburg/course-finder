97571985ac51bbb2e96f6ca0859063e9d16ac58f
singhraj <rajbir@live.de>
2024-04-13 16:46:03 +0200

    add subject tiles

--- packages/ui/src/components/SubjectTile.vue
+++ packages/ui/src/components/SubjectTile.vue
@@ -1,5 +1,54 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useEnrollmentStore } from '@/stores/enrollment'
+
+const enrollmentStore = useEnrollmentStore()
+</script>
 
 <template>
-  <h1>This is the Subject Tile</h1>
+  <VContainer>
+    <VRow justify="center">
+      <VCol
+        v-for="subject in enrollmentStore.filteredSubjects"
+        :key="subject.name"
+        cols="auto"
+      >
+        <VCard
+          :subtitle="subject.prof"
+          :title="subject.name"
+          height="200"
+          width="300"
+        >
+          <template #append><VCheckbox v-model="subject.selected" /></template>
+          <VCardText>
+            <VRow align="end" justify="end" style="height: 110px">
+              <VCol cols="auto" style="text-align: end">
+                <p>
+                  <strong>{{ subject.cp }} CP</strong>
+                </p>
+                <p>
+                  <strong>{{ subject.sws }} SWS</strong>
+                </p>
+                <p v-if="subject.weekly">
+                  {{
+                    new Date(subject.weekly.from).toLocaleDateString([], {
+                      weekday: 'long',
+                      hour: 'numeric',
+                      minute: '2-digit',
+                    }) +
+                    ' - ' +
+                    new Date(subject.weekly.to).toLocaleTimeString([], {
+                      hour: 'numeric',
+                      minute: '2-digit',
+                    })
+                  }}
+                </p>
+                <p v-else-if="subject.meetings">Blockveranstaltung</p>
+                <p v-else>na</p>
+              </VCol>
+            </VRow>
+          </VCardText>
+        </VCard>
+      </VCol>
+    </VRow>
+  </VContainer>
 </template>
--- packages/ui/src/pages/Test.vue
+++ packages/ui/src/pages/Test.vue
@@ -1,22 +1,9 @@
-<script setup lang="ts">
-import { useEnrollmentStore } from '@/stores/enrollment'
-import { ref } from 'vue'
-
-const enrollmentStore = ref(useEnrollmentStore())
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <h1>This page is only for testing purposes</h1>
     <FilterSection />
-    <VList
-      v-for="subject in enrollmentStore.filteredSubjects"
-      :key="subject.name"
-      class="mx-3"
-    >
-      {{
-        subject.name + ' ' + subject.prof + ' ' + subject.cp + ' ' + subject.sws
-      }}
-    </VList>
+    <SubjectTile />
   </div>
 </template>
