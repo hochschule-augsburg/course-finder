0b3332e7a951103769cd2e36e74ab06f0c2d11d3
Vu Duc Minh <duc.minh.vu@hs-augsburg.de>
2024-04-27 16:38:54 +0200

    Added remaining time, and availbable subjects table with always offered option

--- packages/ui/src/pages/Admin.vue
+++ packages/ui/src/pages/Admin.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { useEnrollmentStore } from '@/stores/enrollment'
-import { VBtn } from 'vuetify/components'
+import { VBtn, VTable } from 'vuetify/components'
 
-const enrollmentStore = useEnrollmentStore()
-const currentPhase = enrollmentStore.currentPhase
+const enrollmentStore = useEnrollmentStore();
+const subjects = enrollmentStore.subjects;
+const phase = enrollmentStore.currentPhase;
 
 function formatDate(dateString: string | undefined) {
   if (!dateString) {
@@ -14,15 +15,63 @@ function formatDate(dateString: string | undefined) {
   return formattedDate;
 }
 
+function getRemainingTime(end: string | undefined) {
+  if (!end) {
+    return '';
+  }
+  const currentDT = new Date();
+  const endDT = new Date(end);
+
+  const timeDifferenceMs = endDT.getTime() - currentDT.getTime();
+  const seconds = Math.floor(timeDifferenceMs / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  return `${days} days, ${hours % 24} hours, ${minutes % 60} minutes,`;
+}
 </script>
 
 <template>
-  <h1>Administration</h1>
   <div>
-    <h2>Current phase: {{ currentPhase?.title.en }}</h2>
-    Start: {{ formatDate(currentPhase?.start.toString()) }} <br />
-    End: {{ formatDate(currentPhase?.end.toString()) }}
+    <h1>Administration</h1>
+    <div class="current-phase">
+      <h2>Current phase: {{ phase?.title.en }}</h2>
+      Start: {{ formatDate(phase?.start.toString()) }} <br />
+      End: {{ formatDate(phase?.end.toString()) }} <br />
+      Remaining Time: {{ getRemainingTime(phase?.end.toString()) }}
+    </div>
+    <div>
+      <h2>Available subjects</h2>
+      <VTable>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Professor
+          </th>
+          <th class="text-left">
+            Always offered
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="subject in subjects"
+          :key="subject.moduleCode"
+        >
+          <td>{{ subject.title.en }}</td>
+          <td>{{ subject.allLecturers.toString() }}</td> 
+          <td>
+            <input type="checkbox" :value="subject.moduleCode">
+          </td>
+        </tr>
+      </tbody>
+      <VBtn>Save</VBtn>
+    </VTable>
+    </div>
+    <VBtn text="Create enrollment phase" />
   </div>
-  <VBtn text="Create enrollment phase" />
-  <!-- Comment <SubjectTable /> -->
 </template>
