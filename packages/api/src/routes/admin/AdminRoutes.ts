80f58d9cac1f03263f67bce34e5ab195fff4c524
Niklas Sirch <sirniklax@gmail.com>
2024-05-03 13:53:22 +0200

    add api for assignments

--- packages/api/src/routes/admin/AdminRoutes.ts
+++ packages/api/src/routes/admin/AdminRoutes.ts
@@ -1,8 +1,27 @@
+import { assign } from '../../domain/assign/AssignmentAlgorithm'
+import { prisma } from '../../prisma/prisma'
 import { courseRouter } from '../course/CourseRoutes'
 import { enrollRouter } from '../enroll/EnrollRoutes'
-import { router } from '../trpc'
+import { adminProcedure, router } from '../trpc'
 
 export const adminRouter = router({
+  /**
+   * Temporary development api
+   */
+  assign: adminProcedure.mutation(async () => {
+    const currentPhaseId = (await prisma.enrollphase.findFirst({}))?.id ?? 0
+    const results = await assign(currentPhaseId)
+    prisma.phaseAssignment.createMany({
+      data: Object.entries(results).flatMap(([student, moduleCodes]) =>
+        moduleCodes.map((moduleCode) => ({
+          moduleCode,
+          phaseId: currentPhaseId,
+          username: student,
+        })),
+      ),
+    })
+    return results
+  }),
   courses: courseRouter,
   enroll: enrollRouter,
 })
