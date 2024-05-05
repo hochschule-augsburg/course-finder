a6dd03e3dd8905113b1d199457ab82caea545f99
MiTROSKA <minhvud003@gmail.com>
2024-05-05 16:52:52 +0200

    Started form for offeredcourses

--- packages/ui/src/components/OfferedCourses.vue
+++ packages/ui/src/components/OfferedCourses.vue
@@ -2,7 +2,22 @@
 import type { Subject } from '@/stores/AdminStore'
 
 import { useAdminStore } from '@/stores/AdminStore'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+
+type TimeInterval<T> = { from: T; to: T }
+
+export type CourseAppointmentsJson<T> =
+  | {
+      /**
+       * Ignore days, months and years
+       */
+      dates: Array<TimeInterval<T>>
+      type: 'weekly'
+    }
+  | {
+      dates: Array<TimeInterval<T>>
+      type: 'block' | 'irregular'
+    }
 
 const enrollmentStore = useAdminStore()
 
@@ -46,6 +61,45 @@ function onDrop(event: DragEvent, droppedTable: string): void {
     }
   }
 }
+
+const showModalForm = ref(false)
+const offeredCourseList = []
+
+const selectedSubject = ref<Subject>(enrollmentStore.subjects[0])
+const formData = ref({
+  appointments: {} as CourseAppointmentsJson<Date>,
+  extraInfo: '',
+  maxParticipants: 0,
+  minParticipants: 0,
+  moduleCode: '',
+})
+
+function selectSubject(s: Subject) {
+  selectedSubject.value = s
+  formData.value.moduleCode = s.moduleCode
+  formData.value.appointments = s.offeredCourse.appointments
+  formData.value.extraInfo = s.offeredCourse.extraInfo ?? ''
+  formData.value.maxParticipants = s.offeredCourse.maxParticipants ?? 0
+  formData.value.minParticipants = s.offeredCourse.minParticipants ?? 0
+  showModalForm.value = true
+  console.log(formData.value)
+}
+
+function saveSubject() {
+  selectedSubject.value.offeredCourse.moduleCode = formData.value.moduleCode
+  selectedSubject.value.offeredCourse.appointments = formData.value.appointments
+  selectedSubject.value.offeredCourse.extraInfo = formData.value.extraInfo
+  selectedSubject.value.offeredCourse.maxParticipants =
+    formData.value.maxParticipants
+  selectedSubject.value.offeredCourse.minParticipants =
+    formData.value.minParticipants
+
+  offeredCourseList.push(selectedSubject)
+  showModalForm.value = false
+  // TODO: Change subject in store
+}
+
+// TODO: Send offeredcourselist object to backend
 </script>
 
 <template>
@@ -70,7 +124,7 @@ function onDrop(event: DragEvent, droppedTable: string): void {
           </div>
         </div>
       </VCol>
-      <VCol>
+      <VCol cols="12" lg="6">
         <div
           class="drop-zone"
           @dragenter.prevent
@@ -85,22 +139,85 @@ function onDrop(event: DragEvent, droppedTable: string): void {
             draggable="true"
             @dragstart="startDrag($event, subject, 'table2')"
           >
-            {{ subject.title.en }}
+            {{ subject.title.en }} <a @click="selectSubject(subject)">- Edit</a>
+            <div>
+              {{ subject.offeredCourse.appointments.dates }}
+            </div>
+            <div>
+              {{ subject.offeredCourse.minParticipants }}
+              {{ subject.offeredCourse.maxParticipants }}
+              {{ subject.offeredCourse.extraInfo }}
+            </div>
           </div>
         </div>
       </VCol>
     </VRow>
+    <VDialog
+      v-model="showModalForm"
+      min-width="800"
+      transition="false"
+      width="auto"
+    >
+      <VCard
+        :title="`Edit - ${selectedSubject.title.en}`"
+        prepend-icon="mdi-pencil"
+      >
+        <VCardText>
+          <VRow dense>
+            <VCol cols="12" md="6">
+              <VTextField
+                v-model="formData.minParticipants"
+                label="Minimum participants"
+                type="number"
+                required
+              />
+            </VCol>
+            <VCol cols="12" md="6">
+              <VTextField
+                v-model="formData.maxParticipants"
+                label="Maximum participants"
+                type="number"
+                required
+              />
+            </VCol>
+            <VCol cols="12">
+              <VTextarea
+                v-model="formData.appointments.dates"
+                label="Appointments"
+                required
+              />
+            </VCol>
+            <VCol cols="12">
+              <VTextarea
+                v-model="formData.extraInfo"
+                label="Extra information"
+                required
+              />
+            </VCol>
+          </VRow>
+          <small class="text-caption text-medium-emphasis"
+            >*separate multiple elements with comma</small
+          >
+        </VCardText>
+        <VDivider />
+        <template #actions>
+          <VBtn text="Cancel" @click="showModalForm = false" />
+          <VBtn class="ms-auto" text="Save" @click="showModalForm = false" />
+        </template>
+      </VCard>
+    </VDialog>
   </VContainer>
 </template>
 
 <style scoped lang="scss">
 $backgroundColor: #ecf0f1;
-$itemBackgroundColor: #ff266d;
-$itemColor: #ffffff;
-$paddingValue: 2%;
+$itemBackgroundColor: #ffffff;
+$borderColor: #ff266d;
+$itemColor: #000000;
+$paddingValue: 1%;
 
 .drop-zone {
-  width: 70%;
+  width: 95%;
   background-color: $backgroundColor;
   padding: $paddingValue;
   min-height: 10%;
@@ -108,6 +225,9 @@ $paddingValue: 2%;
 
 .drag-el {
   background-color: $itemBackgroundColor;
+  border-color: $borderColor;
+  border-radius: $paddingValue;
+  border-style: solid;
   color: $itemColor;
   padding: $paddingValue;
 }
