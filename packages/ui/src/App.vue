01d0fab1356d6a2b4d0b7ee8689ca9a87cd01a6e
s3yit3 <160884149+s3yit3@users.noreply.github.com>
2024-05-06 23:18:47 +0200

    Revert "Navbar divided into 3 components"
    This reverts commit a7a7445e2f1da0a368c64950be0a35e7fe6bc786.

--- packages/ui/src/App.vue
+++ packages/ui/src/App.vue
@@ -9,7 +9,7 @@ useUserStore()
 
 <template>
   <VApp>
-    <CustomNavbar />
+    <Navbar />
     <VMain class="my-4 mx-2">
       <ModalDialog />
       <RouterView />
--- packages/ui/src/components/CustomNavbar.vue
+++ packages/ui/src/components/CustomNavbar.vue
@@ -1,8 +1,15 @@
+<!-- eslint-disable prettier/prettier -->
+<!-- eslint-disable prettier/prettier -->
 <script setup lang="ts">
 import { useUserStore } from '@/stores/UserStore'
 import { useDark, useToggle } from '@vueuse/core'
+<<<<<<< HEAD
 import { computed, unref } from 'vue'
 import { VAppBar } from 'vuetify/components'
+=======
+import { computed, ref, unref } from 'vue'
+import { VBtn, VMenu, VToolbarTitle } from 'vuetify/components'
+>>>>>>> e6c6fa2 (Revert "Navbar divided into 3 components")
 
 const isDark = useDark({
   attribute: 'theme',
@@ -21,6 +28,8 @@ function handleLogout() {
 
 import { useDisplay } from 'vuetify'
 
+const drawer = ref(false)
+
 const display = useDisplay()
 
 const isMobile = computed(() => {
@@ -29,14 +38,131 @@ const isMobile = computed(() => {
 </script>
 
 <template>
-  <VAppBar>
-    <template v-if="isMobile">
-      <NavbarMobile />
-    </template>
-    <template v-else>
-      <NavbarDesktop />
-    </template>
-  </VAppBar>
+  <body>
+    <VAppBar>
+      <VToolbarTitle>
+        <span>{{ $t('NavbarTitle') }}</span>
+      </VToolbarTitle>
+
+      <VSpacer />
+
+      <template v-if="isMobile">
+        <VAppBarNavIcon @click.stop="drawer = !drawer" />
+      </template>
+
+      <template v-else>
+        <VBtn text="help" icon>
+          <VIcon size="28">mdi-help-circle-outline</VIcon>
+          <VTooltip activator="parent" location="bottom">
+            {{ $t('Hilfe') }}
+          </VTooltip>
+        </VBtn>
+
+        <template v-if="userStore.user">
+          <VBtn text="user">
+            {{ userStore.user?.name }}
+            <VIcon size="28">mdi-account-circle</VIcon>
+          </VBtn>
+
+          <VBtn text="logout" @click="handleLogout">
+            <VIcon size="28">mdi-logout-variant</VIcon>
+            <VTooltip activator="parent" location="bottom"> Logout </VTooltip>
+          </VBtn>
+        </template>
+        <VBtn v-else text="login">
+          <VIcon size="28">mdi-login-variant</VIcon>
+          <VTooltip activator="parent" location="bottom"> Login </VTooltip>
+
+          <VMenu :close-on-content-click="false" activator="parent">
+            <template #default="{ isActive }">
+              <LoginDialog v-if="isActive" />
+            </template>
+          </VMenu>
+        </VBtn>
+
+        <p class="sprachtext-desktop">{{ $t('Sprachwechsel') }}:</p>
+
+        <VBtn
+          class="en-button"
+          elevation="1"
+          rounded="xl"
+          @click="$i18n.locale = `en`"
+        >
+          en
+        </VBtn>
+        <VBtn
+          class="de-button"
+          elevation="1"
+          rounded="xl"
+          @click="$i18n.locale = `de`"
+        >
+          de
+        </VBtn>
+      </template>
+    </VAppBar>
+    <VNavigationDrawer v-model="drawer" location="right" temporary>
+      <div class="nav-drawer">
+        <VBtn class="help-button" elevation="0" text="help" block>
+          {{ $t('Hilfe') }}
+          <VIcon size="24">mdi-help-circle-outline</VIcon>
+          <VTooltip activator="parent" location="bottom">
+            {{ $t('Hilfe') }}
+          </VTooltip>
+        </VBtn>
+
+        <VDivider class="nav-divider" />
+        <p>is Dark: {{ isDark }}</p>
+        <button @click="toggleDark()">Toggle Dark Mode</button>
+        <VDivider class="nav-divider" />
+        <template v-if="userStore.user">
+          <VBtn text="user">
+            {{ userStore.user?.name }}
+            <VIcon size="28">mdi-account-circle</VIcon>
+          </VBtn>
+
+          <VBtn elevation="0" text="logout" block @click="handleLogout">
+            Logout
+            <VIcon size="28">mdi-logout-variant</VIcon>
+            <VTooltip activator="parent" location="bottom"> Logout </VTooltip>
+          </VBtn>
+        </template>
+        <VBtn v-else elevation="0" text="login" block>
+          Login
+          <VIcon size="28">mdi-login-variant</VIcon>
+          <VTooltip activator="parent" location="bottom"> Login </VTooltip>
+
+          <VMenu :close-on-content-click="false" activator="parent">
+            <template #default="{ isActive }">
+              <LoginDialog v-if="isActive" />
+            </template>
+          </VMenu>
+        </VBtn>
+        <VDivider class="nav-divider" />
+
+        <div class="language-buttons">
+          <p class="Sprachtext">{{ $t('Sprachwechsel') }}:</p>
+
+          <VBtn
+            class="en-button"
+            elevation="1"
+            rounded="xl"
+            @click="$i18n.locale = `en`"
+          >
+            en
+          </VBtn>
+
+          <VBtn
+            class="de-button"
+            elevation="1"
+            rounded="xl"
+            @click="$i18n.locale = `de`"
+          >
+            de
+          </VBtn>
+        </div>
+      </div>
+    </VNavigationDrawer>
+  </body>
 </template>
 
 <style scoped lang="scss">
