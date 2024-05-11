5627142abe66d9b3dfb9fbb32c68e8ff5909f227
Niklas Sirch <sirniklax@gmail.com>
2024-05-11 20:38:30 +0200

    improve admin enrollementphase

--- packages/ui/src/components/admin/EnrollmentPhase.vue
+++ packages/ui/src/components/admin/EnrollmentPhase.vue
@@ -1,43 +1,88 @@
 <script setup lang="ts">
-import { useCoursesStore } from '@/stores/CoursesStore'
+import { useAdminCoursesStore } from '@/stores/admin/AdminCoursesStore'
+import { useIntervalFn } from '@vueuse/core'
+import { formatDuration, intervalToDuration, isWithinInterval } from 'date-fns'
+import { computed, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-const enrollmentStore = useCoursesStore()
-const phase = enrollmentStore.currentPhase
-
-function formatDate(dateString: string | undefined) {
-  if (!dateString) {
-    return ''
+const { locale, t } = useI18n()
+const adminCoursesStore = useAdminCoursesStore()
+const phase = computed(() => {
+  const phase = adminCoursesStore.phases.find((e) =>
+    isWithinInterval(new Date(), {
+      end: new Date(e.end),
+      start: new Date(e.start),
+    }),
+  )
+  if (phase) {
+    return {
+      ...phase,
+      endFormatted: phase.end.toLocaleString(locale.value, {
+        dateStyle: 'short',
+        timeStyle: 'short',
+      }),
+      startFormatted: phase.start.toLocaleString(locale.value, {
+        dateStyle: 'short',
+        timeStyle: 'short',
+      }),
+    }
   }
-  const date = new Date(dateString)
-  const formattedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}, ${date.getHours()}:00`
-  return formattedDate
-}
+  return undefined
+})
 
-function getRemainingTime(end: string | undefined) {
-  if (!end) {
-    return ''
-  }
-  const currentDT = new Date()
-  const endDT = new Date(end)
+const remainingTime = ref<string>()
 
-  const timeDifferenceMs = endDT.getTime() - currentDT.getTime()
-  const seconds = Math.floor(timeDifferenceMs / 1000)
-  const minutes = Math.floor(seconds / 60)
-  const hours = Math.floor(minutes / 60)
-  const days = Math.floor(hours / 24)
+useIntervalFn(
+  () => {
+    const currentDT = new Date()
+    const endDT = phase.value?.end
+    if (!endDT) {
+      return
+    }
 
-  return `${days} days, ${hours % 24} hours, ${minutes % 60} minutes,`
-}
+    //TODO check if locale is used
+    remainingTime.value = formatDuration(
+      intervalToDuration({ end: endDT, start: currentDT }),
+      { format: ['months', 'weeks', 'days', 'hours', 'minutes'], zero: true },
+    )
+  },
+  2000,
+  { immediate: true },
+)
 </script>
 
 <template>
   <div>
-    <h1>Administration</h1>
-    <div class="current-phase">
-      <h2>Current phase: {{ phase?.title.en }}</h2>
-      Start: {{ formatDate(phase?.start.toString()) }} <br />
-      End: {{ formatDate(phase?.end.toString()) }} <br />
-      Remaining Time: {{ getRemainingTime(phase?.end.toString()) }}
+    <h1>{{ t('title') }}</h1>
+    <div v-if="phase" class="current-phase">
+      <h2>
+        {{ t('phase') }}:
+        {{ locale === 'en' ? phase.title.en : phase.title.de }}
+      </h2>
+      {{ t('start') }}:
+      {{ phase.startFormatted }}
+      <br />
+      {{ t('end') }}:
+      {{ phase.endFormatted }}
+      <br />
+      <template v-if="remainingTime">
+        {{ t('remaining-time') }}: {{ remainingTime }}
+      </template>
     </div>
   </div>
 </template>
+
+<i18n lang="yaml">
+en:
+  title: Administration
+  phase: Current phase
+  start: Start
+  end: End
+  remaining-time: Remaining Time
+de:
+  title: Verwaltung
+  phase: Aktuelle Phase
+  start: Start
+  end: Ende
+  remaining-time: Verbleibende Zeit
+</i18n>
