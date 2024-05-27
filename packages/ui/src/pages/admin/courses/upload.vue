78f45ecae678b289fd41861eeb5b1f1f498d5dd2
rajxsingh <rajbir@live.de>
2024-05-27 22:03:12 +0200

    add upload btn :facepalm

--- packages/ui/src/pages/admin/courses/upload.vue
+++ packages/ui/src/pages/admin/courses/upload.vue
@@ -1,20 +1,41 @@
 <script lang="ts" setup>
-import { VCol, VFileInput, VRow } from 'vuetify/components'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+import { VBtn, VCol, VContainer, VFileInput, VRow } from 'vuetify/components'
+
+const { t } = useI18n()
+const router = useRouter()
+const pending = ref(false)
+
+function uploadFile() {
+  pending.value = true
+
+  setTimeout(() => router.back(), 1000)
+}
 </script>
 
 <template>
   <div class="mx-15">
     <VContainer>
-      <VRow justify="center">
-        <VCol>
-          <VFileInput
-            accept=".pdf"
-            label="Select file"
-            placeholder="Select file"
-            outlined
-          />
-        </VCol>
-      </VRow>
+      <VCol justify="space-between">
+        <VFileInput
+          accept=".pdf"
+          label="Select file"
+          placeholder="Select file"
+          outlined
+        />
+        <VRow class="pr-3" justify="end">
+          <VBtn :loading="pending" :text="t('upload')" @click="uploadFile" />
+        </VRow>
+      </VCol>
     </VContainer>
   </div>
 </template>
+
+<i18n lang="yaml">
+en:
+  upload: Upload
+de:
+  upload: Upload
+</i18n>
