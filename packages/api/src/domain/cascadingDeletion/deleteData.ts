3472b41e82016e7745c6a6945ea5792a6f168ea3
Daniel Holnaicher <daniel.holnaicher@hs-augsburg.de>
2024-05-27 15:28:03 +0200

    deleting file

---
+++ packages/api/src/domain/cascadingDeletion/deleteData.ts
@@ -0,0 +1,24 @@
+// index.ts
+import cron from 'node-cron';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function deleteOldStudentData() {
+  const oneAndHalfYearsAgo = new Date();
+  oneAndHalfYearsAgo.setMonth(oneAndHalfYearsAgo.getMonth() - 18); // 1.5 years ago
+
+  try {
+    const result = await prisma.student.deleteMany({
+      where: {
+        createdAt: {
+          lt: oneAndHalfYearsAgo,
+        },
+      },
+    });
+
+    console.log(`Deleted ${result.count} old student records.`);
+  } catch (error) {
+    console.error('Error deleting old student records:', error);
+  }
+}
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,13 +1,19 @@
+import { deleteOldStudentData } from './domain/cascadingDeletion/deleteData'
 import { startPhaseSchedulingFromDatabase } from './domain/phase/PhaseService'
 import { prisma } from './prisma/prisma'
 import { createServer } from './server/server'
+import cron from 'node-cron';
 
-// Start the server
 const server = await createServer()
 await prisma.$connect()
 
 // Start the registration cycle
 startPhaseSchedulingFromDatabase()
+cron.schedule('0 0 1 */6 *', () => {
+        console.log('Running scheduled job to delete old student data');
+        deleteOldStudentData();
+    });
+console.log('Scheduler is set up to run every 6 months.');
 
 await server.start()
 await prisma.$disconnect()
