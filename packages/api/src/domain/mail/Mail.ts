7126f6d020cee9cef2c150d6040bee4e3762273e
nicolasb-github <bota.nicolas@gmail.com>
2024-06-02 22:10:19 +0200

    fix test

--- packages/api/src/domain/mail/Mail.ts
+++ packages/api/src/domain/mail/Mail.ts
@@ -2,28 +2,28 @@ import type { Attachment } from 'nodemailer/lib/mailer'
 
 import nodemailer from 'nodemailer'
 
-if (!process.env.MAIL_SENDER_PASSWORD || !process.env.MAIL_SENDER_USERNAME) {
-  throw new Error(
-    'MAIL_SENDER_USERNAME and MAIL_SENDER_PASSWORD enviroment variables must be set.',
-  )
-}
-
-const transporter = nodemailer.createTransport({
-  auth: {
-    pass: process.env.MAIL_SENDER_PASSWORD,
-    user: process.env.MAIL_SENDER_USERNAME,
-  },
-  host: 'smtp.hs-augsburg.de',
-  port: 25,
-  secure: false, // will still use STARTTLS if the host supports it
-})
-
 export async function sendEmail(
   to: string | string[],
   subject: string,
   text: string,
   attachments?: Attachment[],
 ) {
+  if (!process.env.MAIL_SENDER_PASSWORD || !process.env.MAIL_SENDER_USERNAME) {
+    throw new Error(
+      'MAIL_SENDER_USERNAME and MAIL_SENDER_PASSWORD enviroment variables must be set.',
+    )
+  }
+
+  const transporter = nodemailer.createTransport({
+    auth: {
+      pass: process.env.MAIL_SENDER_PASSWORD,
+      user: process.env.MAIL_SENDER_USERNAME,
+    },
+    host: 'smtp.hs-augsburg.de',
+    port: 25,
+    secure: false, // will still use STARTTLS if the host supports it
+  })
+
   const info = await transporter.sendMail({
     attachments,
     from: 'subject-enroll@tha.de',
