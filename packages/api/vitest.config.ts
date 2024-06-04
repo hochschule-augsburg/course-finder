a0df820b24732a1abb1fb5418226fe8c4eef2dac
nicolasb-github <bota.nicolas@gmail.com>
2024-06-04 14:08:46 +0200

    added envs for tests, added workaround for lint problem

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    env: {
+      DATABASE_URL: 'testDatabaseUrl',
+      FRONTEND_HOSTNAME: 'testFrontendHostname',
+      LDAP_BASE_DN: 'testLdapBaseDn',
+      LDAP_URL: 'testLdapUrl',
+      MAIL_RECEIVERS: 'testMailReceivers',
+      MAIL_SENDER_PASSWORD: 'testMailSenderPassword',
+      MAIL_SENDER_USERNAME: 'testMailSenderUsername',
+      SERVER_HOSTNAME: 'testServerHostname',
+      SERVER_PORT: 'testServerPort',
+      SESSION_SECRET: 'testSessionSecret',
+    },
     environment: 'node',
     globals: true,
     includeSource: ['src/domain/enroll/**/*.ts'],
