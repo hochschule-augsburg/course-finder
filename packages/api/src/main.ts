c391757696425b32603b4fc9eda071fa2ebf7bdc
Niklas Sirch <sirniklax@gmail.com>
2024-10-04 22:12:07 +0200

    remove sass warnings

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -14,7 +14,10 @@ if (process.env.NODE_ENV !== 'production') {
 const server = await createServer()
 await prisma.$connect()
 
-if (process.argv.slice(2).includes('master')) {
+if (
+  process.env.NODE_ENV === 'development' ||
+  process.argv.slice(2).includes('master')
+) {
   console.log('I am the master process')
   try {
     await prisma.appConf.create({})
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -43,6 +43,13 @@ export default defineConfig({
       ),
     }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
