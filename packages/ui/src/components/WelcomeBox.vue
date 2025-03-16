22786f52d572f1e46fbf388dc35fd2d589738ba9
Niklas Sirch <sirniklax@gmail.com>
2025-03-16 17:11:41 +0100

    improve welcome

--- packages/ui/src/components/WelcomeBox.vue
+++ packages/ui/src/components/WelcomeBox.vue
@@ -3,7 +3,7 @@ import { useUserStore } from '@/stores/UserStore'
 import { useI18n } from 'vue-i18n'
 import { VSheet } from 'vuetify/components'
 
-const { t } = useI18n()
+const { locale, t } = useI18n()
 const userStore = useUserStore()
 </script>
 
@@ -17,6 +17,21 @@ const userStore = useUserStore()
     <h2>{{ t('welcome-title') }} {{ userStore.user?.name }}</h2>
     <p>{{ t('welcome-text') }}</p>
     <p v-if="!userStore.user">{{ t('login-text') }}</p>
+    <p v-else-if="userStore.user.Student">
+      <i18n-t keypath="student-more-info">
+        <template #link>
+          <a
+            :href="
+              locale === 'de'
+                ? 'https://www.tha.de/Informatik/Studiengaenge-Informatik.html'
+                : 'https://www.tha.de/en/Computer-Science/Programs.html'
+            "
+          >
+            {{ t('study-programs-link') }}
+          </a>
+        </template>
+      </i18n-t>
+    </p>
     <p
       v-if="
         userStore.user?.Student?.fieldOfStudy ===
@@ -37,9 +52,13 @@ en:
   welcome-text: |
     Welcome to the registration page for elective courses 
     at the Faculty of Computer Science at Augsburg University of Applied Sciences.
-  login-text:
-    Log in with you RZ-Account to register for courses. You can view courses
+  login-text: |
+    Log in with your RZ-Account to register for courses. You can view courses
     without logging in.
+  student-more-info: |
+    This page provides all the information you need to plan your semester.
+    You can find more details on the pages of the {link}.
+  study-programs-link: study programs
   may-enroll-info: |
     Since you are still in the orientation phase, you cannot yet
     register for courses. However, you are welcome to inform yourself
@@ -47,14 +66,19 @@ en:
   ia-no-reg: |
     Since you are studying Interactive Media, you cannot register for courses here.
     However, you can contact the lecturers directly by email to enroll.
+
 de:
   welcome-title: Willkommen
   welcome-text: |
     Willkommen auf der Anmeldeseite für Wahlpflichtfächer 
     der Fakultät für Informatik an der Technischen Hochschule Augsburg.
-  login-text:
-    Melde dich mit deinem RZ-Account an, um dich für Kurse zu registrieren.
-    Die Kursübersicht ist auch ohne Anmeldung verfügbar.
+  login-text: |
+    Melde dich mit deinem RZ-Account an, um dich für Kurse zu registrieren. Die
+    Kursübersicht ist auch ohne Anmeldung verfügbar.
+  student-more-info: |
+    Diese Seite bietet alle Informationen für die Planung deines Semesters.
+    Du kannst dich aber noch genauer auf den Seiten der {link} informieren.
+  study-programs-link: Studiengänge
   may-enroll-info: |
     Da du dich noch in der Orientierungsphase befindest, kannst du dich
     noch nicht für Kurse anmelden. Du kannst dich jedoch gerne schon
